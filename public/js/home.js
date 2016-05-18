75baf764a4380512b09ff138c1190c72cae20c80
hoangkien <hoanglieuphuong@gmail.com>
2016-05-18 22:03:32 +0700

    add Delete

--- public/js/home.js
+++ public/js/home.js
@@ -1,19 +1,33 @@
-function generateContent(no, title, content){
-	return '<div class="well"><h3 class="title">#' + no + ' ' + title +'</h3><hr><p class="para">'+ content + '</p><button class="btn btn-danger" type="submit" name="delete" value="12"><i class="fa fa-trash"></i> Xóa #' + no + '</button></div>';
+function generateContent(no, title, content, id){
+	return '<div class="well"><h3 class="title">#' + no + ' ' + title +'</h3><hr><p class="para">'+ content + '</p><button class="btn btn-danger btndelete" type="submit" name="delete" value="'+ id +'"><i class="fa fa-trash"></i> Xóa #' + no + '</button></div>';
 }
 var i = 1;
+
+function deletepost(){
+	$(this).parent().remove();
+		$.post(window.location.origin + '/post',
+		{
+			'action' : 'delete',
+			'id' : $(this).attr('value')
+		},
+		function(data, status){
+			console.log(status);
+	});
+}
+
 $(document).ready(function(){
 	$.post(window.location.origin + '/post',
 	{
 		'action' : 'getContent'
 	},
 	function(data, status){
-		console.log(data);
+		//console.log(data);
 		data.forEach(function(elem){
-			var html = generateContent(i, elem.title, elem.content);
+			var html = generateContent(i, elem.title, elem.content, elem.id);
 			$("#content").prepend(html);
 			i++;
 		});
+		$(".btndelete").on('click', deletepost);
 	});
 	$("#submit").on('click', function(){
 		var subject = $("#subject").val();
@@ -27,11 +41,11 @@ $(document).ready(function(){
 		}, 
 		function(data, status){
 			if (status=== "success"){
-				var html = generateContent(i, subject, contentText);
+				var html = generateContent(i, subject, contentText, data);
 				$("#content").prepend(html);
 				i++;
+				$(".btndelete").on('click', deletepost);
 			}
 		});
-
 	});
 });
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var bodyparser = require('body-parser');
 var mongo = require('mongodb').MongoClient;
+var ObjectID = require('mongodb').ObjectID;
 var app = express();
 
 app.use(bodyparser.urlencoded({extended: true}));
@@ -21,6 +22,7 @@ app.post('/post', function(req, res){
 				var arr = [];
 				data.forEach(function(doc){
 					var j = {
+						'id' : doc._id,
 						'title' : doc.title,
 						'content' : doc.content
 					};
@@ -45,17 +47,28 @@ app.post('/post', function(req, res){
 			'content' : req.body.content
 			}, function(err, data){
 				if (err) throw err;
+				res.send(data.insertedIds[0]);
 				db.close();
 			});
 		});
-		res.send('Ok');
-		console.log(req.body);
 	}
+	else if (req.body.action === 'delete'){
+		mongo.connect('mongodb://127.0.0.1:27017/diary', function(err, db){
+			if (err) throw err;
+			var collection = db.collection('mypost');
+			collection.remove({ '_id': ObjectID(req.body.id)}, function(err, result){
+				if (err) throw err;
+				//console.log(result);
+				res.end();
+				db.close();
+			});
+		});
+	} 
 	else {
-		res.end();	
+		res.end();
 	}
 });
 
-app.listen(process.argv[2], function(){
+app.listen(process.argv[2] || 8080 , function(){
 	console.log('Server is listening on port ' + process.argv[2]);
 });
